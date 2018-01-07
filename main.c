b45b6df196e6a2bffa7e17bb4ab087ac9d404132
lukasz.pryjda <lukasz.pryjda@sagiton.pl>
2018-01-07 14:19:32 +0100

    wares

--- main.c
+++ main.c
@@ -16,8 +16,44 @@ pthread_mutex_t mutex_day = PTHREAD_MUTEX_INITIALIZER;
 
 typedef struct element {
    struct element *next;
-   char type[6];
+   char *type;
  } wares ;
+
+wares *first;
+
+ void add_to_list (wares *el, char *type)
+ {
+   wares *wsk, *new_item;
+   wsk = el;
+   while (wsk->next != NULL)
+     {
+     wsk = wsk->next;
+     }
+   new_item =(wares*) malloc (sizeof(wares));
+   new_item->type = type;
+   new_item->next = NULL;
+   wsk->next = new_item;
+ }
+
+bool delete_from_list(wares *el, char element[6])
+{
+   wares *wsk=el;
+   while (wsk->next != NULL)
+   {
+     if (wsk->next->type == element)
+     {
+       wares *del=wsk->next;
+       wsk->next = del->next;
+       free(del);
+       return true;
+     }
+     else
+     {
+       wsk = wsk->next;
+     }
+   }
+   return false;
+}
 
 int throwDice(){
     return rand()%6+1;
@@ -28,10 +64,29 @@ void fight();
 void buy_wares();
 void serve();
 
-void *knight(void *vargp);
-void *innkeeper(void *vargp);
-void *lady(void *vargp);
-void *shopkeeper(void *vargp);
+void *knight(void *vargp){
+    usleep(500);
+    pthread_exit(NULL);
+    return NULL;
+};
+
+void *innkeeper(void *vargp){
+    usleep(500);
+    pthread_exit(NULL);
+    return NULL;
+};
+
+void *lady(void *vargp){
+    usleep(500);
+    pthread_exit(NULL);
+    return NULL;
+};
+
+void *shopkeeper(void *vargp){
+    usleep(500);
+    pthread_exit(NULL);
+    return NULL;
+};
 
 void playDay(int d)
 {
@@ -39,7 +94,18 @@ void playDay(int d)
     pthread_t tid2;
     pthread_t tid3;
     pthread_t tid4;
-
+    for(int i=1; i<=innkeepers; i++){
+        pthread_create(&tid1, NULL, innkeeper, NULL);
+    }
+    for(int i=1; i<=knights; i++){
+        pthread_create(&tid2, NULL, knight, NULL);
+    }
+    for(int i=1; i<=shopkeepers; i++){
+        pthread_create(&tid3, NULL, shopkeeper, NULL);
+    }
+    for(int i=1; i<=ladies; i++){
+        pthread_create(&tid4, NULL, lady, NULL);
+    }
     pthread_join(tid1, NULL);
     pthread_join(tid2, NULL);
     pthread_join(tid3, NULL);
@@ -61,6 +127,18 @@ int main(int argc, char *argv[])
     drinks = atoi(argv[6]);
     food = atoi(argv[7]);
     gems = atoi(argv[8]);
+    first =(wares*) malloc (sizeof(wares));
+    first->type = "gem";
+    first->next = NULL;
+    for (int i=gems;i>0;i--) {
+        add_to_list(first,"gem");
+    }
+    for (int i=food;i>0;i--) {
+        add_to_list(first,"food");
+    }
+    for (int i=drinks;i>0;i--) {
+        add_to_list(first,"drink");
+    }
     pthread_mutex_init(&mutex_resources, NULL);
     pthread_mutex_init(&mutex_knight, NULL);
     pthread_mutex_init(&mutex_shopkeeper, NULL);
